746dde2753738ce17b68d9514bfd0a60880a464a
CeDwukropek <emilkopytek@gmail.com>
2023-01-08 21:02:08 +0100

    cleared code

--- App/script.js
+++ App/script.js
@@ -11,88 +11,56 @@ const daysCount = () => {
 const firstDayInMonth = () => {
     return new Date(actualYear, actualMonth).getDay()
 }
+const createPreviousDays = (daysCounter, daysFromPreviousMonth) => {
+    for (let i = 0; i < daysFromPreviousMonth; i++) {
+        let createdDiv = appendChildMultiple(i + 1)
+        let firstDayFromPreviousMonth = daysCount() - 5
+        let pInDiv = createdDiv.getElementsByTagName('p')[0]
+
+        createdDiv.setAttribute('id', firstDayFromPreviousMonth + i)
+        createdDiv.classList.add('previousMonth')
+        pInDiv.innerHTML = firstDayFromPreviousMonth + i
+        daysCounter++
+    }
+
+    return daysCounter
+}
+const createNormalDays = (daysCounter, days) => {
+    for (let i = 0; i < days; i++) {
+        let createdDiv = appendChildMultiple(i + 1)
+
+        createdDiv.setAttribute('id', i + 1)
+        daysCounter++
+    }
+
+    return daysCounter
+}
+function createNextDays(daysCounter, maxDays) {
+    for (let i = daysCounter, j = 1; i < maxDays; i++, j++) {
+        let createdDiv = appendChildMultiple(i + 1)
+        let pInDiv = createdDiv.getElementsByTagName('p')[0]
+
+        createdDiv.setAttribute('id', j)
+        createdDiv.classList.add('nextMonth')
+        pInDiv.innerHTML = j
+    }
+}
 
 function createDays(days) {
     let daysCounter = 0
     let daysFromPreviousMonth = firstDayInMonth() - 1;
-
-    console.log(daysFromPreviousMonth + daysCount())
-
-    let maxDays = daysFromPreviousMonth + daysCount() > (5 * 7) ? (6 * 7) : (5 * 7)
-
+    
+    daysFromPreviousMonth = daysFromPreviousMonth < 0 ? daysFromPreviousMonth = 6 : daysFromPreviousMonth
+    
+    let maxDays = (daysFromPreviousMonth + daysCount()) > (5 * 7) ? (6 * 7) : (5 * 7)
 
     if (firstDayInMonth() == 1) {
-        for (let i = 0; i < days; i++) {
-            let createdDiv = appendChildMultiple(i + 1)
-
-            createdDiv.setAttribute('id', i + 1)
-            daysCounter++
-        }
-
-        for (let i = daysCounter, j = 1; i < maxDays; i++, j++) {
-            let createdDiv = appendChildMultiple(i + 1)
-            let pInDiv = createdDiv.getElementsByTagName('p')[0]
-
-            createdDiv.setAttribute('id', j)
-            createdDiv.classList.add('previousMonth')
-            pInDiv.innerHTML = j
-        }
-    
+        daysCounter += createNormalDays(daysCounter, days)
+        createNextDays(daysCounter, maxDays)
     } else {
-
-        if (daysFromPreviousMonth < 0) {
-            daysFromPreviousMonth = 6
-            for (let i = 0; i < daysFromPreviousMonth; i++) {
-                let createdDiv = appendChildMultiple(i + 1)
-                let firstDayFromPreviousMonth = daysCount() - 5
-                let pInDiv = createdDiv.getElementsByTagName('p')[0]
-
-                createdDiv.setAttribute('id', firstDayFromPreviousMonth + i)
-                createdDiv.classList.add('previousMonth')
-                pInDiv.innerHTML = firstDayFromPreviousMonth + i
-                daysCounter++
-            }
-
-            for (let i = 0; i < days; i++) {
-                let createdDiv = appendChildMultiple(i + 1)
-
-                createdDiv.setAttribute('id', i + 1)
-                daysCounter++
-            }
-
-            for (let i = daysCounter, j = 1; i < maxDays; i++, j++) {
-                let createdDiv = appendChildMultiple(i + 1)
-                let pInDiv = createdDiv.getElementsByTagName('p')[0]
-    
-                createdDiv.setAttribute('id', j)
-                createdDiv.classList.add('previousMonth')
-                pInDiv.innerHTML = j
-            }
-        } else {
-            for (let i = 0; i < daysFromPreviousMonth; i++) {
-                let createdDiv = appendChildMultiple(i + 1)
-
-                createdDiv.setAttribute('id', i + 1)
-                createdDiv.classList.add('previousMonth')
-                daysCounter++
-            }
-
-            for (let i = 0; i < days; i++) {
-                let createdDiv = appendChildMultiple(i + 1)
-
-                createdDiv.setAttribute('id', i + 1)
-                daysCounter++
-            }
-
-            for (let i = daysCounter, j = 1; i < maxDays; i++, j++) {
-                let createdDiv = appendChildMultiple(i + 1)
-                let pInDiv = createdDiv.getElementsByTagName('p')[0]
-    
-                createdDiv.setAttribute('id', j)
-                createdDiv.classList.add('previousMonth')
-                pInDiv.innerHTML = j
-            }
-        }
+        daysCounter += createPreviousDays(daysCounter, daysFromPreviousMonth)
+        daysCounter = createNormalDays(daysCounter, days)
+        createNextDays(daysCounter, maxDays)
     }
 }
 
