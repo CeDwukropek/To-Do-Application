88953eda8f643e69622cfe275a42c39df41bf5f4
CeDwukropek <emilkopytek@gmail.com>
2023-01-12 16:20:18 +0100

    deleted unnecesary JS file

--- App/calendar.js
+++ App/calendar.js
@@ -94,6 +94,31 @@ function createDays(days) {
     document.getElementById(today()).classList.add("today")
 }
 
+//custom shadow BG for days
+const containers = document.querySelectorAll('.day')
+
+//setting values before users move
+for(const el of containers) {
+    const rect = el.getBoundingClientRect(),
+          x = 0 - rect.left,
+          y = 0 - rect.top;
+    
+    el.style.setProperty("--mouse-x", `${x}px`)
+    el.style.setProperty("--mouse-y", `${y}px`)
+}
+
+//setting values when user moves mouse
+document.onmousemove = e => {
+    for(const el of containers) {
+        const rect = el.getBoundingClientRect()
+        const x = e.clientX - rect.left
+        const y = e.clientY - rect.top
+        
+        el.style.setProperty("--mouse-x", `${x}px`)
+        el.style.setProperty("--mouse-y", `${y}px`)
+    }
+}
+
 //run function to create days in calendar
 createDays(daysCount())
 
